aab3f22902eaa359033ffd120d455f0e67581aac
Vinayak Nikam <vinayaknikam2001@gmail.com>
2026-05-08 11:51:23 +0530

    Fresh Start DSA

--- OLD_REPO/PlayGround.cpp
+++ OLD_REPO/PlayGround.cpp
@@ -3,11 +3,11 @@ using namespace std;
 typedef long long int lli;
 typedef unsigned long long ull;
     
-    
 int main()
 {
-    
-    
-    
+    vector<int> vec(10, 2);
+    int *p = &vec[0];
+    vec.push_back(3);
+    cout << *p <<endl;
     return 0;
 }
