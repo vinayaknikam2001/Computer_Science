53ca6b68a77ca980ff29f7af01548d30b630f491
Vinayak Nikam <vinayaknikam2001@gmail.com>
2026-05-21 15:48:53 +0530

    Sliding Window Max

---
+++ DSA/Queue/Pattern/Hard/1_SlidingWindowMax.cpp
@@ -0,0 +1,44 @@
+/*
+LeetCode 239 = https://leetcode.com/problems/sliding-window-maximum/description/
+Date = 14-May-2026
+TC = 
+SC = 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution 
+{
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+    {
+        deque<int> dq;
+        vector <int> vecRes;
+        for (int i=0; i<k; ++i)
+        {
+            while (!dq.empty() && nums[dq.back()] < nums[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        vecRes.push_back(nums[dq.front()]);
+
+        for (int i=k; i<nums.size(); ++i)
+        {
+            while (!dq.empty() && dq.front() <= (i-k))
+            {
+                dq.pop_front();
+            }
+
+            while (!dq.empty() && nums[dq.back()] < nums[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            vecRes.push_back(nums[dq.front()]);
+        }
+
+        return vecRes;
+    }
+};
